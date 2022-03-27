efdf46f87a751ffa002d345b75e716b850f6ae60
Ian <ianchen0419@gmail.com>
2022-03-27 18:13:13 +0800

    Create Day2-tuples.swift

---
+++ Day2-tuples.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+// tuple
+// 1. Can't add or remove item from a tuple, they are fixed
+// 2. Can't change a type of items in a tuple, they always have the same types they were created with
+// 3. You can access items in a tuple using numerical positions, or by naming them
+
+var name = (first: "Taylor", last: "Swift")
+print(name.0)
+print(name.first)
+
+
