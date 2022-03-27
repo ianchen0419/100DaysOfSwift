b7365d00627c0ed49cde02377d49ac8f45528db4
Ian <ianchen0419@gmail.com>
2022-03-28 00:52:45 +0800

    Create Day4-writing-throwing-functions.swift

---
+++ Day4-writing-throwing-functions.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+    
+    return true
+}
