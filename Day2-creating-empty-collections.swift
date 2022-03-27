3050069b55d6a03ca92b3fd9bde8b3c426ebe001
Ian <ianchen0419@gmail.com>
2022-03-27 18:43:13 +0800

    Create Day2-creating-empty-collections.swift

---
+++ Day2-creating-empty-collections.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// empty dictionary with special syntax
+var teams = [String: String]()
+teams["Paul"] = "Red"
+
+// empty array with secial syntax
+var results = [Int]()
+
+// empty set
+var words = Set<String>()
+var numbers = Set<Int>()
+
+// empty dictionary with normal syntax
+var scores = Dictionary<String, Int>()
+
+// empty array with normal syntax
+var result = Array<Int>()
