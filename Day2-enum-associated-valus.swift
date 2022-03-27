8c9bef13fbc0594f2dc29a0eac5e0662141b9a58
Ian <ianchen0419@gmail.com>
2022-03-27 19:04:51 +0800

    Update Day2-enum-associated-valus.swift

--- Day2-enum-associated-valus.swift
+++ Day2-enum-associated-valus.swift
@@ -9,3 +9,20 @@ enum Activity {
 
 let talking = Activity.talking(topic: "football")
 print(talking)
+
+enum Weather {
+    case sunny
+    case windy(speed: Int)
+    case rainy(chance: Int, amount: Int)
+}
+
+// enum with associated value try to resolve problems like this
+enum ComplexWeather {
+    case sunny
+    case lightBreeze
+    case aBitWindy
+    case quiteBlueteryNow
+    case nowThatsAStrongWind
+    case thisIsSeriousNow
+    case itsAHurricane
+}
