a023c68fdcd4424fb6e43a63b82d1f71717f91f5
Ian <ianchen0419@gmail.com>
2022-03-27 22:28:14 +0800

    Create Day4-infinite-loops.swift

---
+++ Day4-infinite-loops.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+import UIKit
+
+var counter = 0
+
+while true {
+    print(" ")
+    counter += 1
+
+    if counter == 273 {
+        break
+    }
+}
+
+//while true {
+//    print("I'm alive") // priting forever
+//}
+//
+//print("I've snuffed it!") // never shown
+
+var isAlive = false
+
+while isAlive == true {
+    print("I'm alive!")
+}
+
+print("I've snuffed it!")
+
