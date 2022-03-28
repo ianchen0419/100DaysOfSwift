b83cdc2debd32a272b2d75a86343fe2890c217c6
Ian <ianchen0419@gmail.com>
2022-03-28 22:38:58 +0800

    Create Day8-properties-and-methods-of-strings.swift

---
+++ Day8-properties-and-methods-of-strings.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+let string = "Do or do not, there is no try."
+
+print(string.count) // 30
+print(string.hasPrefix("Do")) // true
+print(string.uppercased()) // DO OR DO NOT, THERE IS NO TRY.
+print(string.sorted()) // [" ", " ", " ", " ", " ", " ", " ", ",", ".", "D", "d", "e", "e", "h", "i", "n", "n", "o", "o", "o", "o", "o", "r", "r", "r", "s", "t", "t", "t", "y"]
+
