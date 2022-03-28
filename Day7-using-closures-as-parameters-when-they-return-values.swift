a5d2c7e69a3cfb3eab9a746b10231f97236a9c9f
Ian <ianchen0419@gmail.com>
2022-03-28 18:17:04 +0800

    Update Day7-using-closures-as-parameters-when-they-return-values.swift

--- Day7-using-closures-as-parameters-when-they-return-values.swift
+++ Day7-using-closures-as-parameters-when-they-return-values.swift
@@ -10,3 +10,35 @@ func travel(action: (String) -> String) {
 travel { (place: String) -> String in
     return "I'm going to \(place) in my car"
 }
+
+func reduce(_ values: [Int], using closure: (Int, Int) -> Int) -> Int {
+    var current = values[0]
+    
+    for value in values[1...] {
+        current = closure(current, value)
+    }
+    
+    return current
+}
+
+
+let numbers = [10, 20, 30]
+
+let sum = reduce(numbers) { (runningTotal: Int, next: Int) -> Int in
+    runningTotal + next
+}
+
+let sumWithoutTypeAnnotation = reduce(numbers) { (runningTotal, next) in
+    runningTotal + next
+} // can omit closure's type annotation
+
+print(sum)
+print(sumWithoutTypeAnnotation)
+
+let multiplied = reduce(numbers) { (runningTotal: Int, next: Int) in
+    runningTotal * next
+}
+print(multiplied)
+
+let sumNeatly = reduce(numbers, using: +)
+print(sumNeatly)
