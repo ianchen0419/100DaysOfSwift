07cfe2ebfe4ec2de4ad64eab8689fbf425c0855d
Ian <ianchen0419@gmail.com>
2022-03-28 22:22:40 +0800

    Create Day8-property-observers.swift

---
+++ Day8-property-observers.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% completed.")
+        }
+    }
+}
+
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
