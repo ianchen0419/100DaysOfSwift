b756a73072214a606ca444b375a9ec8d91506ce2
Ian <ianchen0419@gmail.com>
2022-03-30 01:45:06 +0800

    Create Day14-classes.swift

---
+++ Day14-classes.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class Singer {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    @objc func sing() {
+        print("La la la la")
+    }
+}
+
+// all the class is available to Objective-C
+@objcMembers class Singer2 {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    func sing() {
+        print("La la la la")
+    }
+}
