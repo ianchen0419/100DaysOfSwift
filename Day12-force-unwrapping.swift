2575caa9ee44903ad20d8e6f62073cd69f4da97f
Ian <ianchen0419@gmail.com>
2022-03-29 13:40:20 +0800

    Update Day12-force-unwrapping.swift

--- Day12-force-unwrapping.swift
+++ Day12-force-unwrapping.swift
@@ -2,3 +2,35 @@ import UIKit
 
 let str = "5"
 let num = Int(str)! // crash operator
+
+let url = URL(string: "https://www.apple.com")! // safe
+//let url = URL(string: "https://www.\(website)")! // unsaefe
+
+let randomNumber = (1...10).randomElement()! // safe
+
+//var items = [Int]()
+//
+//for i in 1...10 {
+//    if isLuckyNumber(i) {
+//        items.append(i)
+//    }
+//}
+//
+//let randomNumber = items.randomElement()! // unsafe
+
+
+enum Direction: CaseIterable {
+    case north, south, east, west
+}
+
+let randomDirection = Direction.allCases.randomElement()! // safe
+
+enum GoodDirection: CaseIterable {
+    case north, south, east, west
+
+    static func random() -> Direction {
+        Direction.allCases.randomElement()! // better solution
+    }
+}
+let GoodRandomDirection = GoodDirection.random()
+
