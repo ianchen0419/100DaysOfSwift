c54fc423c1392e596bb4a53719ec964421f2fd97
Ian <ianchen0419@gmail.com>
2022-03-29 15:26:46 +0800

    Update Day12-typecasting.swift

--- Day12-typecasting.swift
+++ Day12-typecasting.swift
@@ -21,3 +21,27 @@ for pet in pets {
 
 //Woof!
 //Woof!
+
+class Person {
+    var name = "Anonymous"
+}
+
+class Customer: Person {
+    var id = 12345
+}
+
+class Employee: Person {
+    var salary = 50_000
+}
+
+let customer = Customer()
+let employee = Employee()
+let people = [customer, employee]
+
+for person in people {
+    if let customer = person as? Customer {
+        print("I'm a customer, with id \(customer.id)")
+    } else if let employee = person as? Employee {
+        print("I'm a employee, erning $\(employee.salary)")
+    }
+}
