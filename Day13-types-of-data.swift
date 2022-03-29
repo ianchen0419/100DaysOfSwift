1bd382e2fbdad3bfa34d134b6d22f723847729b5
Ian <ianchen0419@gmail.com>
2022-03-29 19:00:50 +0800

    Create Day13-types-of-data.swift

---
+++ Day13-types-of-data.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+/*
+ Double
+ */
+
+var latitude: Double
+latitude = 36.166667 // 36.166667
+
+var longitude: Float
+longitude = -86.783333 // -86.78333
+
+longitude = -186.783333 // -186.7833
+
+longitude = -1286.783333 // -1286.783
+
+longitude = -12386.783333 // -12386.78
+
+longitude = -123486.783333 // -123486.8
+
+longitude = -1234586.783333 // -1234587
+
+var longitude2: Double
+longitude2 = -1234586.783333 // -1234586.783333
+longitude2 = -1234586789012.783333 // -1234586789012.783
+
+/*
+ Type Inference
+ */
+
+// without type inference
+var name: String
+name = "Tim McGraw"
+
+// with type inference
+var name2 = "Aka"
