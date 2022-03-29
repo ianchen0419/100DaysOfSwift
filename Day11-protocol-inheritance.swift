95570d5cf7fd18d511fa168a08bab479e52a2ac7
Ian <ianchen0419@gmail.com>
2022-03-29 09:24:34 +0800

    Update Day11-protocol-inheritance.swift

--- Day11-protocol-inheritance.swift
+++ Day11-protocol-inheritance.swift
@@ -13,3 +13,35 @@ protocol HasVacation {
 }
 
 protocol Employee: Payable, NeedsTraining, HasVacation { }
+
+//protocol Computer {
+//    var price: Double { get set }
+//    var weight: Int { get set }
+//    var cpu: String { get set }
+//    var memory: Int { get set }
+//    var storage: Int { get set }
+//}
+//
+//protocol Laptop {
+//    var price: Double { get set }
+//    var weight: Int { get set }
+//    var cpu: String { get set }
+//    var memory: Int { get set }
+//    var storage: Int { get set }
+//    var screenSize: Int { get set }
+//}
+
+protocol Product {
+    var price: Double { get set }
+    var weight: Int { get set }
+}
+
+protocol Computer: Product {
+    var cpu: String { get set }
+    var memory: Int { get set }
+    var storage: Int { get set }
+}
+
+protocol Laptop: Computer {
+    var screenSize: Int { get set }
+}
