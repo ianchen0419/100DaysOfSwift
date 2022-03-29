7c7d2d74d7950f0e363fd8a456107a9087c12b48
Ian <ianchen0419@gmail.com>
2022-03-29 13:31:08 +0800

    Create Day12-unwrapping-with-guard.swift

---
+++ Day12-unwrapping-with-guard.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+func greet(_ name: String?) {
+    guard let unwrapped = name else {
+        print("You didn't provide a name!")
+        return
+    }
+    
+    print("Hello, \(unwrapped)!")
+}
