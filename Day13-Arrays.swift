e833b20253b8610d7fcd588e11048032354e91eb
Ian <ianchen0419@gmail.com>
2022-03-29 20:57:49 +0800

    Create Day13-Arrays.swift

---
+++ Day13-Arrays.swift
@@ -0,0 +1,4 @@
+import UIKit
+
+var songs: [String] = ["Shake it Off", "You Belong with Me", "Back to December"]
+var songsWithOtherType: [Any] = ["Shake it Off", "You Belong with Me", "Back to December", 3]
