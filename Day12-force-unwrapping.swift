884e20086f7eb1b29fcee059a053ef5f1fe1582d
Ian <ianchen0419@gmail.com>
2022-03-29 14:42:34 +0800

    Update Day12-force-unwrapping.swift

--- Day12-force-unwrapping.swift
+++ Day12-force-unwrapping.swift
@@ -4,33 +4,36 @@ let str = "5"
 let num = Int(str)! // crash operator
 
 let url = URL(string: "https://www.apple.com")! // safe
-//let url = URL(string: "https://www.\(website)")! // unsaefe
+
+let website = ""
+let urlUnsafe = URL(string: "https://www.\(website)")! // unsafe
 
 let randomNumber = (1...10).randomElement()! // safe
 
-//var items = [Int]()
-//
-//for i in 1...10 {
-//    if isLuckyNumber(i) {
-//        items.append(i)
-//    }
-//}
-//
-//let randomNumber = items.randomElement()! // unsafe
+var items = [Int]()
 
+func isLuckyNumber(_ number: Int) -> Bool {
+    number.isMultiple(of: 2)
+}
+
+for i in 1...10 {
+    if isLuckyNumber(i) {
+        items.append(i)
+    }
+}
+
+let randomNumberUnsafe = items.randomElement()! // unsafe
 
 enum Direction: CaseIterable {
     case north, south, east, west
+    
+    static func random() -> Direction {
+        Direction.allCases.randomElement()!
+    }
 }
 
 let randomDirection = Direction.allCases.randomElement()! // safe
 
-enum GoodDirection: CaseIterable {
-    case north, south, east, west
+let randomDirectionBetter = Direction.random() // safe and better!
 
-    static func random() -> Direction {
-        Direction.allCases.randomElement()! // better solution
-    }
-}
-let GoodRandomDirection = GoodDirection.random()
 
