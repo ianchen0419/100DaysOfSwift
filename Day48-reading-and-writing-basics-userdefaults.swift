0e803caae6e58263b6fe168bc54bf3484a0ada3e
Yi An Chen <ianchen0419@gmail.com>
2022-04-09 22:01:21 +0800

    Create Day48-reading-and-writing-basics-userdefaults.swift

---
+++ Day48-reading-and-writing-basics-userdefaults.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  TestOnly
+//
+//  Created by Yi An Chen on 2022/4/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let defaults = UserDefaults.standard
+        
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "UseFaceID")
+        defaults.set(CGFloat.pi, forKey: "Pi")
+        
+        defaults.set("Paul Hudson", forKey: "Name")
+        defaults.set(Date(), forKey: "LastRun")
+        
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "SavedArray")
+        
+        let dict = ["Name": "Paul", "Country": "UK"]
+        defaults.set(dict, forKey: "SavedDictionary")
+        
+        
+        let savedInteger = defaults.integer(forKey: "Age") // if not exist, return 0
+        let savedBoolean = defaults.bool(forKey: "UseFaceID")
+        
+        let savedArray = defaults.object(forKey: "SavedArray") as? [String] ?? [String]()
+        let savedDictionary = defaults.object(forKey: "SavedDictionary") as? [String: String] ?? [String: String]()
+        
+        let savedArray2 = defaults.array(forKey: "SavedArray") // [Any]?
+        let savedDictionary2 = defaults.dictionary(forKey: "SavedDictionary") // [String: Any]?
+    }
+
+
+}
+
