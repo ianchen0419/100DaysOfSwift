bbba2b70763c5d2b586e65156aefd12ccd7a8a77
Yi An Chen <ianchen0419@gmail.com>
2022-04-25 17:29:49 +0800

    Create Day64-basic-swift-debugging-using-print.swift

---
+++ Day64-basic-swift-debugging-using-print.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Debugging
+//
+//  Created by 陳怡安 on 2022/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        print("I'm inside the viewDidLoad() method.")
+        print(1, 2, 3, 4, 5, separator: "-") // 1-2-3-4-5
+        print("Some message", terminator: "@") // Some message@
+        
+    }
+    
+
+}
+
